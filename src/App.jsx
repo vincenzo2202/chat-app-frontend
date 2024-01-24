f7991365275db0f9acac0b26c872ef7d56cf8474
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2024-01-24 12:06:36 +0100

    feat: added form to login and register usin bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import {Routes, Route, Navigate} from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Chat from './pages/Chat/Chat';
 import Register from './pages/Register/Register';
 import Login from './pages/Login/Login';
-function App() { 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap'
+import NavBar from './components/NavBar/NavBar';
+
+function App() {
 
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Chat/>}/> 
-      <Route path='/register' element={<Register/>}/> 
-      <Route path='/Login' element={<Login/>}/> 
-      <Route path='*' element={<Navigate to='/'/>}/> 
-    </Routes>
+    <NavBar />
+    <Container >
+      <Routes>
+        <Route path='/' element={<Chat />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/Login' element={<Login />} />
+        <Route path='*' element={<Navigate to='/' />} />
+      </Routes>
+    </Container>
     </>
   )
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,25 @@
+import { Container, ListGroup, Nav, Navbar, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+    return (
+        <Navbar bg="dark" className="mb-4" style={{ height: "3.75rem" }}>
+            <Container>
+                <h2>
+                     <Link to='/' className="link-light text-decoration-none">Chat App</Link>
+                </h2>
+                <span className="text-warning">Welcome Vincenzo</span>
+                <Nav>
+                    <Stack direction="horizontal" gap={3}>
+                        <Link to='/login' className="link-light text-decoration-none">Login</Link>
+                        <Link to='/register' className="link-light text-decoration-none">Register</Link>
+                        <Link to='/' className="link-light text-decoration-none"></Link>
+                        <Link to='/' className="link-light text-decoration-none"></Link>
+                    </Stack>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default NavBar;
