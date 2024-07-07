b27431bb386fd7eeff9fa998c244069aa7122d53
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2024-07-07 17:49:11 +0200

    FEAT: added Functionality to send the message when pressing Enter

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { AuthContext } from "../../Context/AuthContext";
 import { ChatContext } from "../../Context/ChatContext";
 import { useFetchRecipientUser } from "../../hooks/useFetchRecipient";
@@ -6,57 +6,74 @@ import { Stack } from "react-bootstrap";
 import moment from "moment";
 import InputEmoji from "react-input-emoji";
 
-// P@ssw0rd123!
-
 const ChatBox = () => {
     const { user } = useContext(AuthContext);
-    const { currentChat, messages,isMessagesLoading,sendTextMessage} = useContext(ChatContext); 
+    const { currentChat, messages, isMessagesLoading, sendTextMessage } = useContext(ChatContext);
     const { recipientUser } = useFetchRecipientUser(currentChat, user);
-    const [textMessage, setTextMessage] = useState(''); 
- 
+    const [textMessage, setTextMessage] = useState('');
+    const scroll = useRef();
+
+    useEffect(() => {
+        setTimeout(() => {
+            scroll.current?.scrollIntoView({ block: "end" });
+        }, 0.01); // Ajusta el tiempo de retraso según sea necesario
+    }, [messages]);
+
+    // Manejar el evento de tecla "Enter"
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter' && !event.shiftKey) {
+            event.preventDefault(); // Evita el salto de línea si se presiona "Enter"
+            sendTextMessage(textMessage, user, currentChat._id, setTextMessage);
+        }
+    };
+
     if (!recipientUser) {
         return <p style={{ textAlign: "center", width: "100%" }}>No conversation selected ...</p>;
     }
-    
+
     if (isMessagesLoading) {
         return <p style={{ textAlign: "center", width: "100%" }}>Loading messages...</p>;
     }
 
-    return (<Stack gap={4} className="chat-box">
-        <div className="chat-header">
-            <strong>{recipientUser?.name}</strong>
-        </div>
-        <Stack gap={3} className="messages">
-            {messages && messages.map((message, index) => {
-                return (
-                    <Stack key={index} className={`${message?.senderId === user?._id
-                        ? "message self align-self-end flex-grow-0"
-                        : "message align-self-start flex-grow-0"}`}>
-                        <span>{message.text}</span>
-                        <span className="message-footer">{moment(message.createdAt).calendar()}</span>
-                    </Stack>)
-            })}
+    return (
+        <Stack gap={4} className="chat-box">
+            <div className="chat-header">
+                <strong>{recipientUser?.name}</strong>
+            </div>
+            <Stack gap={3} className="messages">
+                {messages && messages.map((message, index) => {
+                    return (
+                        <Stack key={index} className={`${message?.senderId === user?._id
+                            ? "message self align-self-end flex-grow-0"
+                            : "message align-self-start flex-grow-0"}`}
+                            ref={scroll}
+                        >
+                            <span>{message.text}</span>
+                            <span className="message-footer">{moment(message.createdAt).calendar()}</span>
+                        </Stack>)
+                })}
+            </Stack>
+            <Stack direction="horizontal" gap={3} className="chat-input flex-grow-0">
+                <InputEmoji
+                    value={textMessage}
+                    onChange={setTextMessage}
+                    fontFamily="nunito"
+                    borderColor="rgba(72,112,223,0.2)"
+                    onKeyDown={handleKeyDown} // Asignar el controlador de eventos aquí
+                    cleanOnEnter // Opción para limpiar el campo después de presionar Enter
+                    placeholder="Escribe tu mensaje..."
+                />
+                <button 
+                    className="send-btn"
+                    onClick={() => sendTextMessage(textMessage, user, currentChat._id, setTextMessage)}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send-fill" viewBox="0 0 16 16">
+                        <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z" />
+                    </svg>
+                </button>
+            </Stack>
         </Stack>
-        <Stack direction="horizontal" gap={3} className="chat-input flex-grow-0">
-            <InputEmoji
-                value={textMessage}
-                onChange={setTextMessage}
-                fontFamily="nunito"
-                borderColor="rgba(72,112,223,0.2)"></InputEmoji>
-            <button 
-            className="send-btn"
-            onClick={()=> 
-             sendTextMessage(textMessage, user, currentChat._id, setTextMessage)
-             }>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send-fill" viewBox="0 0 16 16">
-                    <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z" />
-                </svg>
-            </button>
-
-        </Stack>
-    </Stack>
     )
-
 }
 
-export default ChatBox; 
+export default ChatBox;
